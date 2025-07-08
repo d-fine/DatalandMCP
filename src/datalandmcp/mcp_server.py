514b447999404bc80ae63c261b43fa8a97a296c4
d93084 <david.leemueller@d-fine.com>
2025-07-08 18:09:29 +0200

    Add dataclient to MCP Server and tool to retrieve SFDR data.

--- src/datalandmcp/mcp_server.py
+++ src/datalandmcp/mcp_server.py
@@ -1,15 +1,33 @@
-# server.py
+#!\bin\bash
 from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp.prompts import base
+
+from datalandmcp.dataland_client import PRODUCTION_INSTANCE, DatalandClient
+from dataland_backend.models.sfdr_data import SfdrData
+from dataland_backend.models.data_type_enum import DataTypeEnum
+
+DatalandClient.set_global_client(PRODUCTION_INSTANCE.client)
+client=DatalandClient.get_global_client()
 
 # Create an MCP server
-mcp = FastMCP("Demo")
+mcp = FastMCP("DatalandMCP")
 
 
-# Add an addition tool
 @mcp.tool()
-def get_scope_emissions(name: str) -> str:
-    """Retrieve scope emissions for a given company name"""
-    return "200000 tonnes CO2e"
+def get_sfdr_data(company_name: str, fiscal_year: str) -> SfdrData:
+    """
+    Retrieve the SFDR reports for a given company name and fiscal year.
+    """
+    company = client.company_api.get_companies(search_string=company_name, data_types=[DataTypeEnum.SFDR])[0]
+    company_id = company.company_id
+    # Fetch SFDR data
+    sfdr_data = client.sfdr_api.get_all_company_sfdr_data(company_id=company_id, reporting_period=fiscal_year)[0].data
+    return sfdr_data
+
+@mcp.prompt(title="Data Format")
+def data_request() -> list[base.Message]:
+    """This prompt gives an additionally requests the LLM to present the data in table format."""
+    return [base.UserMessage("Please present your findings in table format.")]
 
 
 # Add a dynamic greeting resource
@@ -17,3 +35,6 @@ def get_scope_emissions(name: str) -> str:
 def get_greeting(name: str) -> str:
     """Get a personalized greeting"""
     return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    mcp.run()
