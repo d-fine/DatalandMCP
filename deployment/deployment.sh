1e2879f90e09314d3529ead06a13d7698354235f
d93084 <david.leemueller@d-fine.com>
2025-10-06 10:33:39 +0200

    Update CD and deployment.sh

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -11,6 +11,10 @@ while [[ $# -gt 0 ]]; do
       USER="$2"
       shift 2
       ;;
+    --api-key)
+      API_KEY="$2"
+      shift 2
+      ;;
     --help)
       echo "Usage: $0 [--host HOST] [--user USER] [--api-key DATALAND_API_KEY]"
       exit 0
@@ -23,23 +27,19 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-echo "Deploying DatalandMCP to EC2..."
+REMOTE_PATH="/home/${USER}/DatalandMCP"
 
-# Update and install Docker and Docker Compose
-ssh -i "$SSH_KEY" "${USER}@${HOST}" "sudo apt update && sudo apt upgrade -y && sudo apt install docker.io docker-compose-plugin -y"
-
-# Pull the latest DatalandMCP image
-ssh -i "$SSH_KEY" "${USER}@${HOST}" "sudo docker pull ghcr.io/d-fine/datalandmcp:latest"
+echo "Deploying DatalandMCP to ${USER}@${HOST}..."
 
 # Create the directory if it doesn't exist
-ssh -i "$SSH_KEY" "${USER}@${HOST}" "[ -d /home/${USER}/DatalandMCP ] || mkdir -p /home/${USER}/DatalandMCP"
+ssh "${USER}@${HOST}" "[ -d ${REMOTE_PATH} ] || mkdir -p ${REMOTE_PATH}"
 
-# Secure copy the .env and docker-compose.yml files
-scp -i "$SSH_KEY" .env.github "${USER}@${HOST}:/home/${USER}/DatalandMCP/.env"
-scp -i "$SSH_KEY" docker-compose.yml "${USER}@${HOST}:/home/${USER}/DatalandMCP/docker-compose.yml"
+# Create the .env file
+ssh "${USER}@${HOST}" "printf 'DATALAND_API_KEY=${API_KEY}' > ${REMOTE_PATH}/.env"
+ssh "${USER}@${HOST}" "chmod 600 ${REMOTE_PATH}/.env"
 
-# Make the .env file readable only by the owner
-ssh -i "$SSH_KEY" "${USER}@${HOST}" "chmod 600 /home/${USER}/DatalandMCP/.env"
+# Copy the docker-compose.yml file
+scp docker-compose.yml "${USER}@${HOST}:${REMOTE_PATH}/docker-compose.yml"
 
 # Change to working directory and start DatalandMCP service
-ssh -i "$SSH_KEY" "${USER}@${HOST}" "cd /home/${USER}/DatalandMCP && sudo docker compose --profile mcp up -d"
+ssh "${USER}@${HOST}" "cd ${REMOTE_PATH} && sudo docker compose --profile mcp up -d"
